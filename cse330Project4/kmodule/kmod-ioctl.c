24018e7f05910aeeee6c5b2a4a7c6c10a06a0f78
Devinj48 <93933407+Devinj48@users.noreply.github.com>
2024-05-01 01:34:04 -0700

    Update kmod-ioctl.c

--- cse330Project4/kmodule/kmod-ioctl.c
+++ cse330Project4/kmodule/kmod-ioctl.c
@@ -29,87 +29,61 @@
 static dev_t            dev = 0;
 static struct class*    kmod_class;
 static struct cdev      kmod_cdev;
+static struct block_device *g_bdevice;
+
 
 /* Buffers for different operation requests */
 struct block_rw_ops         rw_request;
 struct block_rwoffset_ops   rwoffset_request;
 
-static long kmod_ioctl(struct file *f, unsigned int cmd, unsigned long arg) {
-    void __user *argp = (void __user *)arg;
-    int ret = 0;
-
-    switch (cmd) {
-        case BREAD:
-        case BWRITE: {
-            /* Get request from user */
-            if (copy_from_user(&rw_request, argp, sizeof(rw_request))) {
-                return -EFAULT;
-            }
-
-            /* Allocate a kernel buffer to read/write user data */
-            char *kernel_buffer = kmalloc(rw_request.size, GFP_KERNEL);
-            if (!kernel_buffer) {
-                return -ENOMEM;
-            }
-
-            /* Perform the block operation */
-            if (cmd == BREAD) {
-                ret = perform_block_operation(REQ_OP_READ, rw_request.data, rw_request.size, kernel_buffer);
-            } else {
-                if (copy_from_user(kernel_buffer, rw_request.data, rw_request.size)) {
-                    kfree(kernel_buffer);
-                    return -EFAULT;
-                }
-                ret = perform_block_operation(REQ_OP_WRITE, kernel_buffer, rw_request.size, NULL);
-            }
-
-            kfree(kernel_buffer);
-            break;
-        }
-        case BREADOFFSET:
-        case BWRITEOFFSET: {
-            /* Get request from user */
-            if (copy_from_user(&rwoffset_request, argp, sizeof(rwoffset_request))) {
-                return -EFAULT;
-            }
-
-            /* Allocate a kernel buffer to read/write user data */
-            char *kernel_buffer = kmalloc(rwoffset_request.size, GFP_KERNEL);
-            if (!kernel_buffer) {
-                return -ENOMEM;
-            }
-
-            /* Perform the block operation */
-            if (cmd == BREADOFFSET) {
-                ret = perform_block_operation(REQ_OP_READ, rwoffset_request.data, rwoffset_request.size, kernel_buffer, rwoffset_request.offset);
-            } else {
-                if (copy_from_user(kernel_buffer, rwoffset_request.data, rwoffset_request.size)) {
-                    kfree(kernel_buffer);
-                    return -EFAULT;
-                }
-                ret = perform_block_operation(REQ_OP_WRITE, kernel_buffer, rwoffset_request.size, NULL, rwoffset_request.offset);
-            }
-
-            kfree(kernel_buffer);
-            break;
-        }
-        default:
-            printk("Error: incorrect operation requested, returning.\n");
-            return -ENOTTY;
-    }
-
-    return ret;
-}
-
-static int perform_block_operation(int op, char *user_buffer, int size, char *kernel_buffer, int offset = 0) {
+static int perform_block_operation(int op, char *user_buffer, int size, char *kernel_buffer, int offset) {
     int ret;
-    struct page *page;
-
+    int current_offset;
+    int current_size;
+    int copy_size;
+    int count = 0;
+    struct bio *bdevice_bio;
+    printk("op: %d, size: %d, offset: %d\n", op, size, offset);
     /* Set bio parameters */
-    bio_reset(bdevice_bio);
-    bdevice_bio->bi_iter.bi_sector = offset >> 9; // Convert offset to sector
-    bdevice_bio->bi_opf = op;
-
+    
+    bdevice_bio = bio_alloc(g_bdevice, 1, op, GFP_NOIO);
+    if (!bdevice_bio) {
+        printk(KERN_ERR "Failed to allocate bio\n");
+        return -EIO;
+    }
+    bio_set_dev(bdevice_bio, g_bdevice);
+    
+    current_size = size;
+    current_offset = offset;
+    while(current_size)
+    {
+	copy_size = min(current_size, 512);
+	bdevice_bio->bi_iter.bi_sector = (current_offset/512);
+    	bdevice_bio->bi_opf = op;
+    	bio_add_page(bdevice_bio, vmalloc_to_page(kernel_buffer+current_offset), copy_size, (count*512)); //investigate, count offset from 0
+    	//printk("copy size: %d, current offset: %d, sector: %d\n",copy_size, current_offset, bdevice_bio->bi_iter.bi_sector);
+    	submit_bio_wait(bdevice_bio);
+    	if(op == REQ_OP_READ) {
+    		copy_to_user(user_buffer+current_offset, kernel_buffer+current_offset, copy_size);
+    
+    	}
+    	bio_reset(bdevice_bio, g_bdevice, op);  
+    	
+    	count = (count+1)%8;
+    	//kfree(kernel_buffer);
+    	current_size -= copy_size;
+    	current_offset += copy_size;
+    }
+    
+    
+    
+    
+    
+    
+   
+    
+    bio_put(bdevice_bio);
+#if 0
     /* Add pages to bio */
     while (size > 0) {
         int bytes = min(size, PAGE_SIZE);
@@ -136,9 +110,79 @@ static int perform_block_operation(int op, char *user_buffer, int size, char *ke
         printk(KERN_ERR "Failed to submit bio: %d\n", ret);
         return ret;
     }
+	#endif
+    return ret;
+}
+
+static long kmod_ioctl(struct file *f, unsigned int cmd, unsigned long arg) {
+    void __user *argp = (void __user *)arg;
+    int ret = 0;
+
+    switch (cmd) {
+        case BREAD:
+        case BWRITE: {
+            /* Get request from user */
+            if (copy_from_user(&rw_request, argp, sizeof(rw_request))) {
+                return -EFAULT;
+            }
+
+            /* Allocate a kernel buffer to read/write user data */
+            char *kernel_buffer = kmalloc(max(rw_request.size,PAGE_SIZE), GFP_KERNEL);
+            if (!kernel_buffer) {
+                return -ENOMEM;
+            }
+
+            /* Perform the block operation */
+            if (cmd == BREAD) {
+                ret = perform_block_operation(REQ_OP_READ, rw_request.data, rw_request.size, kernel_buffer, 0);
+            } else {
+                if (copy_from_user(kernel_buffer, rw_request.data, rw_request.size)) {
+                    kfree(kernel_buffer);
+                    return -EFAULT;
+                }
+                //printk("kernel buffer: %s \n",kernel_buffer);
+                ret = perform_block_operation(REQ_OP_WRITE, rw_request.data, rw_request.size, kernel_buffer, 0);
+            }
+
+            kfree(kernel_buffer);
+            break;
+        }
+        case BREADOFFSET:
+        case BWRITEOFFSET: {
+            /* Get request from user */
+            if (copy_from_user(&rwoffset_request, argp, sizeof(rwoffset_request))) {
+                return -EFAULT;
+            }
+
+            /* Allocate a kernel buffer to read/write user data */
+            char *kernel_buffer = kmalloc(max(rwoffset_request.size, PAGE_SIZE), GFP_KERNEL);
+            if (!kernel_buffer) {
+                return -ENOMEM;
+            }
+
+            /* Perform the block operation */
+            if (cmd == BREADOFFSET) {
+                ret = perform_block_operation(REQ_OP_READ, rwoffset_request.data, rwoffset_request.size, kernel_buffer, rwoffset_request.offset);
+            } else {
+                if (copy_from_user(kernel_buffer, rwoffset_request.data, rwoffset_request.size)) {
+                    kfree(kernel_buffer);
+                    return -EFAULT;
+                }
+                ret = perform_block_operation(REQ_OP_WRITE, rwoffset_request.data, rwoffset_request.size, kernel_buffer, rwoffset_request.offset);
+            }
+
+            kfree(kernel_buffer);
+            break;
+        }
+        default:
+            printk("Error: incorrect operation requested, returning.\n");
+            return -ENOTTY;
+    }
 
     return ret;
 }
+ 
+
 
 
 static int kmod_open(struct inode* inode, struct file* file) {
@@ -160,8 +204,8 @@ static struct file_operations fops =
 };
 
 /* Initialize the module for IOCTL commands */
-bool kmod_ioctl_init(void) {
-
+bool kmod_ioctl_init(struct block_device *bdevice) {
+	g_bdevice = bdevice;
     /* Allocate a character device. */
     if (alloc_chrdev_region(&dev, 0, 1, "usbaccess") < 0) {
         printk("error: couldn't allocate \'usbaccess\' character device.\n");
